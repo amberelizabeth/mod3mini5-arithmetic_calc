1629ee01ea0c0e91a30fac7504c3b720abf5ad14
Amber Wetherill <amber.wetherill@gmail.com>
2022-02-28 21:05:29 -0800

    Added POST method that receives three parameters as per assignment Step 4

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: false})); //middleware 
 
-app.use(express.urlencoded({extended: false})); //middleware
 
-
-app.listen(3000, function(){
+app.listen(3000, () =>{
     console.log('Server listening on port 3000....');
 });
+
+
+app.post('/', (req,res) =>{
+    let operation = req.query.operation;
+    let value_1 = parseInt(req.query.value_1);
+    let value_2 = parseInt(req.query.value_2);
+    let isSuccess = true;
+    let resultStr = "Operation: ";
+    let result = 0;
+
+    if( operation == "add" ){
+        resultStr += "Addition";
+
+    }else if( operation == "sub"){
+        resultStr += "Subtraction";
+
+    }else if( operation == "mul"){
+        resultStr += "Multipy";
+
+    }else if( operation == "div"){
+        resultStr += "Divide";
+
+    }else{
+        isSuccess = false;
+    }
+
+    if(isSuccess){
+        resultStr += "\nValue 1: " +value_1+ "\nValue 2: " +value_2+ "\nResult: " + result;
+        console.log(resultStr);
+        res.send('Successfully completed operation.\n' + resultStr);
+    }else{
+        console.log('Invalid operation.');
+        res.send('Invalid operation.');
+    }
+});
+
